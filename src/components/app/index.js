36283527821cc852e72c07f60e9fb13c532030af
Taylor Stemple <taylorstemple@gmail.com>
2018-04-12 10:11:05 -0700

    initial commit

---
+++ src/components/app/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+
+class App extends React.Component{
+  render() {
+    return(
+      <main className='app-container'>
+        <h1>Pro_Mgmt</h1>
+
+      </main>
+    )
+  }
+}
+
+export default App;
