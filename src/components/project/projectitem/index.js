d72bc3a5910fff14293718e599bd8e2a6ff61664
David Kosmos <dakos@Davids-MacBook-Pro-2.local>
2018-04-16 16:13:28 -0700

    initial project item done

---
+++ src/components/project/projectitem/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {projectUpdateRequest, projectDeleteRequest} from '../../../action/project-actions.js'
+import ProjectForm from '../projectform';
+
+class ProjectItem extends React.Component{
+  render(){
+    let {org, project, projectUpdate, projectDelete} = this.props;
+    return(
+      <section className='project-item'>
+        <div className='content'>
+          <h4>{project.projectName}</h4>
+          <p>{project.desc}</p>
+          <p>Start Date: {project.startDate}</p>
+          <p>Due Date: {project.dueDate}</p>
+        </div>
+        <div className='edit'>
+          <ProjectForm 
+            buttonText='Update Project'
+            org={org}
+            project={project}
+            onComplete={projectUpdate} />
+        </div>
+      </section>
+    )
+  }
+}
+
+let mapDispatchToProps = dispatch => ({
+  projectUpdate: project => dispatch(projectUpdateRequest(project)),
+  projectDelete: project => dispatch(projectDeleteRequest(project)),
+})
+
+export default connect(null, mapDispatchToProps)(ProjectItem);
