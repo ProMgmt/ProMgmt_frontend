8252c7bfe72837fe6512e2c2e99ae271414368f8
nicoleweese <nweese@ymail.com>
2018-04-18 14:28:34 -0700

    profile form stuff added, submit does not work yet

--- src/components/profile/myprofile/index.js
+++ src/components/profile/myprofile/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ProfileForm from '../profileform';
+import * as profileActions from '../../../action/profile-actions.js';
 
 class MyProfile extends React.Component {
   
   render() {
+    console.log(this.props.user);
     return(
       <div className='profile'>
-        <h3>{`Hello ${this.props.profile.firstName} ${this.props.profile.LastName}!`}</h3>
-        <ProfileForm />
+        <h1>Test</h1>
+        {this.props.profile ?
+          <h3>{`Hello ${this.props.profile.firstName} ${this.props.profile.LastName}!`}</h3>
+          : null}
+        <ProfileForm user={this.props.user} buttonText='Create a Profile' onComplete={this.props.profileCreateRequest}/>
       </div>
     )
   }
@@ -16,8 +21,14 @@ class MyProfile extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    profile = state.profile,
+    user: state.user,
+    profile: state.profile,
   }
 }
 
-export default connect(mapStateToProps, null)(MyProfile);
+const mapDispatchToProps = dispatch => ({
+  userOrgSet: () => dispatch(orgActions.userOrgEtAllSetRequest()),
+  profileCreateRequest: (profile) => dispatch(profileActions.profileCreateRequest(profile)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyProfile);
