448a14404f9797e23ba9b62e09fb016e7437a044
nicoleweese <nweese@ymail.com>
2018-04-18 13:29:56 -0700

    connect imported from react redux

--- src/components/profile/myprofile/index.js
+++ src/components/profile/myprofile/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ProfileForm from '../profileform';
 
 class MyProfile extends React.Component {
@@ -19,4 +20,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default MyProfile;
+export default connect(mapStateToProps, null)(MyProfile);
