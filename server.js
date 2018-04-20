f65926f803ecac309a1b63c19772f4f3bfda0b74
Taylor Stemple <taylorstemple@gmail.com>
2018-04-20 14:34:10 -0700

    i dunno

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(`${__dirname}/build`));
 
+app.get('*', (request, response) =>
+  response.sendFile(`${__dirname}/build/index.html`));
+
 app.listen(PORT, () => {
   console.log(`Server up on port ${PORT}`);
 });
