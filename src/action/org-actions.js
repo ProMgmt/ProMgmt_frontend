0ab6c02bc5ad28d60124f7c09e5e53fe83d27393
David Kosmos <dakos@Davids-MBP-2.localdomain>
2018-04-19 17:13:25 -0700

    removed auth console log

--- src/action/org-actions.js
+++ src/action/org-actions.js
@@ -22,7 +22,6 @@ export const orgUpdate = org => ({
 
 export const userOrgEtAllSetRequest = () => (dispatch, getState) => {
   let {auth} = getState();
-  console.log('auth', auth);
   return superagent.get(`${__API_URL__}/api/org/user/me`)
     .set('Authorization', `Bearer ${auth}`)
     .then(res => {
