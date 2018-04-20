7e4d6e2dbf1e8b96cf3e49b3c642db9710873134
nicoleweese <nweese@ymail.com>
2018-04-20 13:02:12 -0700

    woo

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -4,16 +4,20 @@ import React from 'react';
 class Landing extends React.Component {
   render() {
     return (
-      <div className='landing'>
-        <h3 className='title'>Welcome to Pro_Mgmt!</h3>
-        <br/><br/><br/>
-        <p>This project management application gives you one place for businesses to help streamline and enhance teamwork within organizations.</p> 
-        <br/><br/>
-        <p>Employees can sign up, create organizations, and add their team members to organizations with or without admin priveledges. Admins are able to update and remove organizations.</p>
-        <br/><br/>
-        <p>Within each organization, projects can be created, with individual admins for each project as well. These admins can create or update existing projects. Every member of a project is able to create tasks for the projects.</p>
-        <br/><br/>
-        <p>This application also has task functionality. Individual tasks can be added to projects, independent or dependent upon each other. </p>
+      <div>
+        <div className='landing'>
+          <h3 className='title'>Welcome to Pro_Mgmt!</h3>
+          <br /><br />
+          <p className='landing-paragraph'>This project management application gives you one place for businesses to help streamline and enhance teamwork within organizations.</p>
+          
+          <p className='landing-paragraph'>Employees can sign up, create organizations, and add their team members to organizations so that they can collaboratively tackle the projects and tasks at hand.</p>
+
+          <p className='landing-paragraph'>The application allows admin functionality on top of user functionality, so that the organization owners can better regulate activities within the organization.</p>
+
+          <p className='landing-paragraph'>In addition, the application provides you with a task gantt chart, so that you can better visualize your progress as your teammates progress.</p>
+
+          <p className='landing-paragraph'>This application was created by Taylor Stemple, Katy Robinson, David Kosmos, and Nicole Weese.</p>
+        </div>
       </div>
     )
   }
